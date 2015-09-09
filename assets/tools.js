826ddc7b5a427f61b1cf773b0a5711afb6d2e88f
Sander Laarhoven <lesander@users.noreply.github.com>
2015-09-09 15:54:24 +0200

    Create tools.js

---
+++ assets/tools.js
@@ -0,0 +1,31 @@
+/**
+ * Magister Calendar v1.0.0
+ * https://git.io/magister
+ *
+ * Copyright 2015 Sander Laarhoven
+ * Licensed under MIT (http://git.io/magister-calendar-license)
+ */
+
+
+module.exports = {
+  validjson: function (string) {
+    try {
+      JSON.parse(string);
+    }
+    catch (e) {
+      return false;
+    }
+    return true;
+  },
+  log: function(status, text) {
+    if (status == "error") {
+      var prefix = "!";
+    }
+    else {
+      var prefix = "*";
+    }
+    var date = new Date();
+    var time = date.getHours() + ":" + date.getMinutes();
+    console.log("[" + prefix + "] " + time + " " + text);
+  }
+}
